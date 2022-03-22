cbf888cc11f23d0522e5dba1f4ea365e16676c4c
sharminlaboni <sharminlaboni18103058@gmail.com>
2022-03-22 15:56:06 +0600

    customer update

--- app/Http/Controllers/backend/CustomerController.php
+++ app/Http/Controllers/backend/CustomerController.php
@@ -9,7 +9,7 @@ use App\Models\Customer;
 class CustomerController extends Controller
 {
     public function Customer(){
-        $customers= Customer::all();
+        $customers = Customer::all();
         return view('backend.pages.Customer.Customerlist',compact('customers'));
        }
        
@@ -19,9 +19,9 @@ class CustomerController extends Controller
     public function CustomerStore(Request $request){
         Customer::create([
             // coloum name of db || name of input field
-            'Customername'=> $request->Customername,
-            'Customercontact'=> $request->Customercontact,
-            'Customeremail'=> $request->Customeremail,
+            'customername'=> $request->customername,
+            'customercontact'=> $request->customercontact,
+            'customeremail'=> $request->customeremail,
 
             
             
